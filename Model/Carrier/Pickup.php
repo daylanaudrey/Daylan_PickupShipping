85da0a037c16a40cac6fb4355b187d642e15037f
daylanaudrey <daylan.audrey@gmail.com>
2025-06-25 22:29:49 -0300

    primeiro commit

---
+++ Model/Carrier/Pickup.php
@@ -0,0 +1,55 @@
+<?php
+namespace Daylan\PickupShipping\Model\Carrier;
+
+use Magento\Quote\Model\Quote\Address\RateRequest;
+use Magento\Shipping\Model\Carrier\AbstractCarrier;
+use Magento\Shipping\Model\Carrier\CarrierInterface;
+
+class Pickup extends AbstractCarrier implements CarrierInterface
+{
+    protected $_code = 'pickupshipping';
+    protected $_isFixed = true;
+
+    protected $_rateResultFactory;
+    protected $_rateMethodFactory;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
+        \Magento\Shipping\Model\Rate\ResultFactory $rateResultFactory,
+        \Psr\Log\LoggerInterface $logger,
+        array $data = []
+    ) {
+        $this->_rateMethodFactory = $rateMethodFactory;
+        $this->_rateResultFactory = $rateResultFactory;
+        parent::__construct($scopeConfig, $logger, $data);
+    }
+
+    public function collectRates(RateRequest $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $result = $this->_rateResultFactory->create();
+        $method = $this->_rateMethodFactory->create();
+
+        $method->setCarrier($this->_code);
+        $method->setCarrierTitle($this->getConfigData('title'));
+
+        $method->setMethod($this->_code);
+        $method->setMethodTitle($this->getConfigData('name'));
+
+        $method->setPrice(0);
+        $method->setCost(0);
+
+        $result->append($method);
+
+        return $result;
+    }
+
+    public function getAllowedMethods()
+    {
+        return [$this->_code => $this->getConfigData('name')];
+    }
+}
